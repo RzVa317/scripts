82f66dcaa8d1d45dacae66a236577958bdcf3147
RzVa317 <ray@rzva.net>
2023-04-17 10:24:45 -0400

    Output of pacman -Qi sent to /dev/null

--- bash/app_installer.sh
+++ bash/app_installer.sh
@@ -15,22 +15,22 @@ visual-studio-code-bin
 nordvpn
 "
 
-#echo "pacman apps are: $pacman_apps"
-#echo "yay apps are: $yay_apps"
-
 read -p "Install useful pacman apps as sudo? (y/n) " yn
 
 case $yn in
 	y )
-    # iterates through pacman_apps
+    # Iterate through pacman_apps
     for app in $pacman_apps ; do
-        # Checking for $app
-        if pacman -Qi $app > /dev/null ; then
+        # Checking for $app, for clarity:
+        # > /dev/null sends response (app installed) to the void
+        # 2> /dev/null sends error (app not installed) to the void
+        if pacman -Qi $app > /dev/null 2> /dev/null ; then
             # Installed
             echo "$app is already installed"
         else
             # Not installed
-            sudo pacman -S $app
+            #sudo pacman -S $app
+            echo "$app is not installed"
         fi
     done;;
 	n ) echo "Not installing anything";;
