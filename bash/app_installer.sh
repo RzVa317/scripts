87b5f040171ef82c37e9a6e15b544d1ab9724f93
RzVa317 <ray@rzva.net>
2023-04-17 12:08:47 -0400

    Cleaned up comments and misc items

--- bash/app_installer.sh
+++ bash/app_installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Apps to install w/ pacman
 pacman_apps="obsidian
 barrier
 tmux
@@ -9,19 +10,22 @@ smb4k
 solaar
 yakuake
 "
+
+# Apps from AUR to install w/ yay
 yay_apps="cryptomator-bin
 remmina-git
 visual-studio-code-bin
 nordvpn
 "
 
-read -p "Install useful pacman apps as sudo? (y/n) " yn
+# Prompt to install pacman_apps
+read -p "Install useful apps w/ pacman? (y/n) " yn
 
 case $yn in
 	y )
     # Iterate through pacman_apps
     for app in $pacman_apps ; do
-        # Checking for $app, for clarity:
+        # Checking for app in $pacman_apps, for clarity:
         # > /dev/null sends response (app installed) to the void
         # 2> /dev/null sends error (app not installed) to the void
         if pacman -Qi $app > /dev/null 2> /dev/null ; then
@@ -34,5 +38,4 @@ case $yn in
         fi
     done;;
 	n ) echo "Not installing anything";;
-	* ) echo continue;;
 esac
