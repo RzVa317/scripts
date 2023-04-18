64f52581710682f19775c8a492eb669752d5a8c8
RzVa317 <ray@rzva.net>
2023-04-18 09:24:03 -0400

    More comments and checks

--- bash/ssh-agent-finder.sh
+++ bash/ssh-agent-finder.sh
@@ -2,13 +2,20 @@
 # This script will search for a running ssh-agent instance and connect to it
 # If not, will start a new instance
 
-echo "Searching running processes for ssh-agent"
+echo "Checking for ssh-agent process"
 # If pgrep doesn't return an ssh-agent process for $USER
 # pgrep outputs the pids of processes found, so sending to the void
 if ! pgrep -u $USER ssh-agent > /dev/null; then
-	echo "No processes found for ssh-agent"
-	# Start new instance
-	eval `ssh-agent`
+	# Start new instance and save env variables
+	echo "Starting new instance"
+	ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
 else
-	echo "ssh-agent process found, will add to env"
+	echo "ssh-agent already running"
+fi
+# At this point an ssh-agent instance should be running
+# If its env variables aren't located, source them
+echo "Checking for SSH_AUTH_SOCK"
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+	echo "Not found, sourcing ssh-agent.env"
+	source "$XDG_RUNTIME_DIR/ssh-agent.env" > /dev/null
 fi
