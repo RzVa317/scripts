96d404f8a68d8502e922b27b9d264fc6ea651460
RzVa317 <ray@rzva.net>
2023-04-18 15:34:58 -0400

    Should be working but isn't

--- bash/ssh-agent-finder.sh
+++ bash/ssh-agent-finder.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-# This script will search for a running ssh-agent instance and connect to it
-# If not, will start a new instance
-
-echo "Checking for ssh-agent process"
-# If pgrep doesn't return an ssh-agent process for $USER
-# pgrep outputs the pids of processes found, so sending to the void
-if ! pgrep -x ssh-agent; then
-	# Start new instance and save env variables
-	echo "Starting new instance"
-	ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
+# This script will search for $SSH_AGENT_PID
+# If not found will start new instance of ssh-agent and save to env
+echo "Searching for ${SSH_AGENT_PID}"
+if [[ ! -n "${SSH_AGENT_PID}" ]]; then
+	echo "SSH_AGENT_PID is not set, starting new instance of ssh-agent"
+	eval "$(ssh-agent)"
+	echo "sourced: ${SSH_AGENT_PID}"
+	export SSH_AGENT_PID
+	export SSH_AUTH_SOCK
 else
-	echo "ssh-agent already running"
-fi
-# At this point an ssh-agent instance should be running
-# If its env variables aren't located, source them
-echo "Checking for SSH_AUTH_SOCK"
-if [[ ! "$SSH_AUTH_SOCK" ]]; then
-	echo "Not found, sourcing ssh-agent.env"
-	cat $XDG_RUNTIME_DIR/ssh-agent.env
-	source $XDG_RUNTIME_DIR/ssh-agent.env
+	echo "SSH_AGENT_PID is set to ${SSH_AGENT_PID}"
 fi
