dde160f5ecd2740029231b82db95a748ca3c9134
RzVa317 <ray@rzva.net>
2023-04-18 20:44:03 -0400

    Removed initial prompt

--- bash/chezmoi/run_onchange_app_installer.sh
+++ bash/chezmoi/run_onchange_app_installer.sh
@@ -24,56 +24,43 @@ remmina-git
 visual-studio-code-bin
 nordvpn"
 
-# Prompt to install pacman_apps
-read -p "Install useful apps w/ pacman? (y/n) " yn
-case $yn in
-	y )
-    # Iterate through pacman_apps
-    for app in $pacman_apps; do
-        # Checking for app in $pacman_apps, for clarity:
-        # > /dev/null sends response (app installed) to the void
-        # 2> /dev/null sends error (app not installed) to the void
-        if pacman -Qi $app > /dev/null 2> /dev/null; then
-            # Installed
-            echo "$app is already installed"
-        else
-            # app is not installed, prompt for install
-            read -p "Install $app? (y/n) " yn
-            case $yn in
-            y )
-            # Install and accept default options
-            sudo pacman -S --noconfirm $app;;
-            n )
-            # Skip install
-            echo "Skipping $app installation";;
-            esac
-        fi
-    done;;
-	n ) echo "Not installing anything through pacman";;
-esac
+for app in $pacman_apps; do
+    # Checking for app in $pacman_apps, for clarity:
+    # > /dev/null sends response (app installed) to the void
+    # 2> /dev/null sends error (app not installed) to the void
+    if pacman -Qi $app > /dev/null 2> /dev/null; then
+        # Installed
+        echo "$app is already installed"
+    else
+        # app is not installed, prompt for install
+        read -p "Install $app? (y/n) " yn
+        case $yn in
+        y )
+        # Install and accept default options
+        sudo pacman -S --noconfirm $app;;
+        n )
+        # Skip install
+        echo "Skipping $app installation";;
+        esac
+    fi
+done
 
-# Prompt to install yay_apps
-read -p "Install useful apps w/ yay? (y/n) " yn
-case $yn in
-    y )
-    # Iterate through yay_apps
-    for app in $yay_apps; do
-        # If $app is installed
-        if yay -Qi $app > /dev/null 2> /dev/null; then
-            # Installed
-            echo "$app is installed already"
-        else
-            # app is not installed, prompt for install
-            read -p "Install $app? (y/n) " yn
-            case $yn in
-            y )
-            # Install and accept default options
-            yay -S --noconfirm $app;;
-            n )
-            # No install
-            echo "Skipping $app installation";;
-            esac
-        fi
-    done;;
-    n ) echo "Not installing anything through yay";;
-esac
+# Iterate through yay_apps
+for app in $yay_apps; do
+    # If $app is installed
+    if yay -Qi $app > /dev/null 2> /dev/null; then
+        # Installed
+        echo "$app is installed already"
+    else
+        # app is not installed, prompt for install
+        read -p "Install $app? (y/n) " yn
+        case $yn in
+        y )
+        # Install and accept default options
+        yay -S --noconfirm $app;;
+        n )
+        # No install
+        echo "Skipping $app installation";;
+        esac
+    fi
+done
