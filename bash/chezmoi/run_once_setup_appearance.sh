0049db01a753bd025142f669f0ba72ab8c9b72b9
Ray Ramirez <ray@rzva.net>
2023-05-18 00:08:44 -0400

    Install Global theme and SDDM theme

--- bash/chezmoi/run_once_setup_appearance.sh
+++ bash/chezmoi/run_once_setup_appearance.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # TODO Add check for DE and only install when running on KDE
-# This script will copy Nordic.KDE.tar.xz to theme dir and extract
-cd ~/.local/share/plasma/desktoptheme
+# 	Test for tar, if exists, skip
+
+# This script will copy Nordic theme files to install dirs and extract
+theme_dir=/usr/share/plasma/look-and-feel
+cd "$theme_dir"
 # Copy from source dir to destination dir
-cp $(chezmoi source-path)/Files/Nordic.KDE.tar.xz .
+sudo cp $(chezmoi source-path)/Files/Nordic.KDE.tar.xz .
 # Extract theme
-tar -xvf Nordic.KDE.tar.xz
+sudo tar -xvf Nordic.KDE.tar.xz
+# Set correct permissions recursively
+sudo chown -R root:root Nordic
+
+# Install SDDM theme
+sddm_dir=/usr/share/sddm/themes/
+cd "$sddm_dir"
+sudo cp $(chezmoi source-path)/Files/Nordic.KDE.SDDM.tar.xz .
+sudo tar -xvf Nordic.KDE.SDDM.tar.xz
