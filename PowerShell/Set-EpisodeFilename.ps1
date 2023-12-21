0e8fef6d575dba6be4392deecb94d153ce4c2f46
Ray Ramirez <ray@rzva.net>
2023-12-21 07:18:59 -0500

    Remove extra var

--- PowerShell/Set-EpisodeFilename.ps1
+++ PowerShell/Set-EpisodeFilename.ps1
@@ -1,5 +1,4 @@
 $Season = "S02";$List = ls;$Count = 1;$List.foreach{
-    $Episode = $_.split($Season)[0];
     $ext = $_.split(".")[-1];
     $FileName = "Episode $Season"+"E";
     $FileName += ("{0:D2}" -f $count) + ".$ext";
