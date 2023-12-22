bd89c5f77af77fddee79abd18e42423791188a86
Ray Ramirez <ray@rzva.net>
2023-12-22 13:53:25 -0500

    Change to S01

--- PowerShell/Set-EpisodeFilename.ps1
+++ PowerShell/Set-EpisodeFilename.ps1
@@ -1,4 +1,4 @@
-$Season = "S02";$List = ls;$Count = 1;$List.foreach{
+$Season = "S01";$List = ls;$Count = 1;$List.foreach{
     $ext = $_.split(".")[-1];
     $FileName = "Episode $Season"+"E";
     $FileName += ("{0:D2}" -f $count) + ".$ext";
